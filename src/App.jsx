1cd6d090db48909f7acca07e624be8f916f8d382
Rejin Nepal <rejinnepal815@gmail.com>
2022-09-06 17:47:26 -0500

    Completed the functionality for increment and decrement function using usEstate

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,32 @@ function App() {
    * https://blog.webdevsimplified.com/2020-04/use-state/
    * 
   */
+  const [count, setCount] = useState(4);
 
   const decrementCount = () => {
     // TODO: Decrement count by 1. 
+    
+    setCount((preCount)=>{
+      return preCount - 1;
+    })
+
+    // setCount(preCount => preCount - 1);
+    // setCount(count - 1);
   }
 
   const incrementCount = () => {
     // TODO: Increse count by 1. 
+
+    setCount((preCount) => {
+      return preCount + 1;
+    })
   }
+
   return (
     <div className="App">
       <button onClick={decrementCount}>-</button>
-      <h1>{/* TODO add Count */}</h1>
-      <button>+</button>
+      <h1>Count: {count}</h1>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
